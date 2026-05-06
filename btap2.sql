9e469b592a9e83f6688b85ba9f2fc9c61974c813
Phan <phanbao15042007@gmail.com>
2026-05-06 13:04:58 +0700

    z

--- btap2.sql
+++ btap2.sql
@@ -1,4 +1,4 @@
+CREATE DATABASE ss05_bt02;
+USE ss05_bt02;
 SELECT restaurant_name, created_at
-FROM Restaurants
-ORDER BY created_at DESC
-LIMIT 5;
+FROM Restaurants ORDER BY created_at DESC LIMIT 5;
